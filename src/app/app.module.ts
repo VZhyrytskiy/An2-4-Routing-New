a608495ada08f7247d4ea628632a503683d93d1c
Vitaliy Zhyrytskyy <Vitaliy_Zhyrytskyy@epam.com>
2016-11-23 17:42:36 +0200

    app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,37 @@ import { NgModule, NgModuleFactoryLoader } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
+// Step 01. In case if we don't have access to index.html file
+// or <head> tag we add base tag programatially
+import { APP_BASE_HREF } from '@angular/common';
+
+// Step 04. Import routing
+import { routing } from './app.routing';
+
 import { TodoAppComponent } from './app.component';
+// Step 04. Import components
+import { HomeComponent } from './components/home';
+import { AboutComponent } from './components/about';
+import { PageNotFoundComponent } from './components/page-not-found';
 
 @NgModule({
   declarations: [
-    TodoAppComponent
+    TodoAppComponent,
+    // Step 04
+    HomeComponent,
+    AboutComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
     CommonModule,
-    FormsModule
+    FormsModule,
+    // Step 04
+    routing
   ],
   providers: [
+    // Step 01
+    // { provide: APP_BASE_HREF, useValue: '/' }
   ],
   entryComponents: [TodoAppComponent],
   bootstrap: [TodoAppComponent]
