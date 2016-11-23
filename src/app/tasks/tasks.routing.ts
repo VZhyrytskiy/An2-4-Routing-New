dc41e1f438fe579f5601daa2882b79f974874ada
Vitaliy Zhyrytskyy <Vitaliy_Zhyrytskyy@epam.com>
2016-11-23 17:59:31 +0200

    tasks_feature_routing

---
+++ src/app/tasks/tasks.routing.ts
@@ -0,0 +1,13 @@
+import { ModuleWithProviders } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { TaskListComponent } from './task-list';
+
+const tasksRoutes: Routes = [
+  {
+    path: 'task-list',
+    component: TaskListComponent
+  }
+];
+
+export const tasksRouting: ModuleWithProviders = RouterModule.forChild(tasksRoutes);
