c034080ad278b7720dce48b18408890f9bfba077
Vitaliy Zhyrytskyy <Vitaliy_Zhyrytskyy@epam.com>
2016-11-24 16:45:42 +0200

    users_nested_routing

--- src/app/users/users.module.ts
+++ src/app/users/users.module.ts
@@ -2,6 +2,9 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 
+import { usersRouting } from './users.routing';
+
+import { UsersComponent } from './users.component';
 import { UserListComponent } from './user-list/user-list.component';
 import { UserFormComponent } from './user-form/user-form.component';
 import { UserComponent } from './user/user.component';
@@ -12,8 +15,10 @@ import { UserArrayService } from './user-array-service/user-array.service';
   imports: [
     CommonModule,
     FormsModule,
+    usersRouting
   ],
   declarations: [
+    UsersComponent,
     UserListComponent,
     UserFormComponent,
     UserComponent,
