c7f39dc319af24fb78a7f04b4857e461859f49e9
Vitaliy Zhyrytskyy <Vitaliy_Zhyrytskyy@epam.com>
2017-03-13 12:51:40 +0200

    add base tag programmatically

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule, NgModuleFactoryLoader } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+
+// add this line if you don't have access to
+// index.html and you want to set base tag
+//import { APP_BASE_HREF } from '@angular/common';
 
 import { TodoAppComponent } from './app.component';
 
@@ -15,6 +19,9 @@ import { TodoAppComponent } from './app.component';
     FormsModule
   ],
   providers: [
+    // add this line if you don't have access to
+    // index.html and you want to set base tag
+    // { provide: APP_BASE_HREF, useValue: '/' }
   ],
   entryComponents: [TodoAppComponent],
   bootstrap: [TodoAppComponent]
