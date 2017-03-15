778fed935110f60918c6f6e17741013d22a6a90c
Vitaliy Zhyrytskyy <Vitaliy_Zhyrytskyy@epam.com>
2017-03-15 13:06:18 +0200

    step_05

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+
 
 // add this line if you don't have access to
 // index.html and you want to set base tag
@@ -19,6 +21,7 @@ import { TodoAppComponent } from './app.component';
     BrowserModule,
     CommonModule,
     FormsModule,
+    RouterModule,
     AppRoutingModule
   ],
   providers: [
