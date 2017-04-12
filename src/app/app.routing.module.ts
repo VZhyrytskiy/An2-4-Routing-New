e365742fb9651a6991e79b6f038d5139b60bee57
Vitaliy Zhyrytskyy <Vitaliy_Zhyrytskyy@epam.com>
2017-04-12 13:32:41 +0300

    step_33

--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -8,21 +8,27 @@ import { CustomPreloadingStrategyService } from './services';
 const routes: Routes = [
   {
     path: 'about',
-    component: AboutComponent
+    component: AboutComponent,
+    data: { title: 'About' }
   },
   {
     path: 'login',
-    component: LoginComponent
+    component: LoginComponent,
+    data: { title: 'Login' }
   },
   {
     path: 'admin',
     canLoad: [AuthGuard],
-    loadChildren: 'app/admin/admin.module#AdminModule'
+    loadChildren: 'app/admin/admin.module#AdminModule',
+    data: { title: 'Admin' }
   },
   {
     path: 'users',
     loadChildren: 'app/users/users.module#UsersModule',
-    data: { preload: true }
+    data: {
+      preload: true,
+      title: 'Users'
+    }
   },
   {
     path: '',
@@ -33,7 +39,8 @@ const routes: Routes = [
     // The router will match this route if the URL requested
     // doesn't match any paths for routes defined in our configuration
     path: '**',
-    component: PageNotFoundComponent
+    component: PageNotFoundComponent,
+    data: { title: 'Page Not Found' }
   }
 ];
 
