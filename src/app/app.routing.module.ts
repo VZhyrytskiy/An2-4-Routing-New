9460125db4d3b863a1fae786c100d517d5ec64e1
Vitaliy Zhyrytskyy <Vitaliy_Zhyrytskyy@epam.com>
2017-08-17 10:01:53 +0300

    secondary router outlet

--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import { AboutComponent, PageNotFoundComponent } from './components';
+import { AboutComponent, MessagesComponent, PageNotFoundComponent } from './components';
 
 const routes: Routes = [
   {
     path: 'about',
     component: AboutComponent
+  },
+  {
+    path: 'messages',
+    component: MessagesComponent,
+    outlet: 'popup'
   },
   {
     path: '',
