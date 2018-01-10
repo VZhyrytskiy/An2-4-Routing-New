7828d471da6a22c10bf2a73c3392f30d444a5034
Vitaliy Zhyrytskyy <Vitaliy_Zhyrytskyy@epam.com>
2018-01-10 13:39:06 +0200

    fix16

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
+import { CoreModule } from './core/core.module';
 import { TasksModule } from './tasks/tasks.module';
 import { Router } from '@angular/router';
 
@@ -11,26 +12,23 @@ import { Router } from '@angular/router';
 
 import { AppRoutingModule, appRouterComponents } from './app.routing.module';
 import { AppComponent } from './app.component';
-import { MessagesService } from './services';
-import { MessagesComponent } from './components';
 
 @NgModule({
   declarations: [
     AppComponent,
-    appRouterComponents,
-    MessagesComponent
+    appRouterComponents
   ],
   imports: [
     BrowserModule,
     FormsModule,
     TasksModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CoreModule
   ],
   providers: [
     // add this line if you don't have access to
     // index.html and you want to set base tag
     // { provide: APP_BASE_HREF, useValue: '/' }
-    MessagesService
   ],
   bootstrap: [AppComponent]
 })
