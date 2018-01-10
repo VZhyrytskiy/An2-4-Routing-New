86f18706ff91f9cac8d00122c24b98271d41e93d
Vitaliy Zhyrytskyy <Vitaliy_Zhyrytskyy@epam.com>
2018-01-10 12:24:47 +0200

    fix13

--- src/app/tasks/task-list/task-list.component.ts
+++ src/app/tasks/task-list/task-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 import { Task } from './../models/task.model';
 import { TaskArrayService } from './../services/task-array.service';
@@ -11,6 +12,7 @@ export class TaskListComponent implements OnInit {
   tasks: Array<Task>;
 
   constructor(
+    private router: Router,
     private taskArrayService: TaskArrayService) { }
 
   ngOnInit() {
@@ -22,7 +24,8 @@ export class TaskListComponent implements OnInit {
   }
 
   editTask(task: Task): void {
-    
+    const link = ['/edit', task.id];
+    this.router.navigate(link);
   }
 
   private async getTasks() {
