34c2fdc5dde8d9bcfef2f689842ae5d5a95722e4
Vitaliy Zhyrytskyy <Vitaliy_Zhyrytskyy@epam.com>
2018-02-13 10:37:26 +0200

    fix19

--- src/app/users/users-routing.module.ts
+++ src/app/users/users-routing.module.ts
@@ -1,7 +1,31 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { UsersComponent, UserListComponent, UserFormComponent } from '.';
+
+const routes: Routes = [
+  {
+    path: 'users',
+    component: UsersComponent,
+    children: [
+      {
+        path: 'add',
+        component: UserFormComponent
+      },
+      {
+        path: 'edit/:userID',
+        component: UserFormComponent,
+      },
+      {
+        path: '',
+        component: UserListComponent
+      },
+    ]
+  }
+];
+
+export let usersRouterComponents = [UsersComponent, UserListComponent, UserFormComponent];
+
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
