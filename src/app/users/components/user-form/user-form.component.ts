d36ef536f71d78def6175193ab49de74fe7762f0
Vitaliy Zhyrytskyy <Vitaliy_Zhyrytskyy@epam.com>
2018-05-17 13:49:31 +0300

    fix17

--- src/app/users/components/user-form/user-form.component.ts
+++ src/app/users/components/user-form/user-form.component.ts
@@ -2,15 +2,15 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
 
 // rxjs
-import { switchMap } from 'rxjs/operators';
-import { Subscription } from 'rxjs/Subscription';
+// import { switchMap } from 'rxjs/operators';
+import { Subscription } from 'rxjs';
 
 import { User } from './../../models/user.model';
 import { UserArrayService } from './../../services/user-array.service';
 
 @Component({
   templateUrl: './user-form.component.html',
-  styleUrls: ['./user-form.component.css'],
+  styleUrls: ['./user-form.component.css']
 })
 export class UserFormComponent implements OnInit, OnDestroy {
   user: User;
@@ -20,39 +20,37 @@ export class UserFormComponent implements OnInit, OnDestroy {
 
   constructor(
     private userArrayService: UserArrayService,
-    private route: ActivatedRoute,
-  ) { }
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
     this.user = new User(null, '', '');
 
     // we should recreate component because this code runs only once
     const id = +this.route.snapshot.paramMap.get('userID');
-    this.sub = this.userArrayService.getUser(id)
-      .subscribe(
-        user => {
-          this.user = {...user};
-          this.originalUser = {...user};
-        },
-        err => console.log(err)
-      );
+    this.sub = this.userArrayService.getUser(id).subscribe(
+      user => {
+        this.user = { ...user };
+        this.originalUser = { ...user };
+      },
+      err => console.log(err)
+    );
   }
 
   ngOnDestroy() {
     this.sub.unsubscribe();
   }
 
-  saveUser() {
-    const user = {...this.user};
+  onSaveUser() {
+    const user = { ...this.user };
 
     if (user.id) {
       this.userArrayService.updateUser(user);
     } else {
       this.userArrayService.addUser(user);
     }
-    this.originalUser = {...this.user};
+    this.originalUser = { ...this.user };
   }
 
-  goBack() {
-  }
+  goBack() {}
 }
