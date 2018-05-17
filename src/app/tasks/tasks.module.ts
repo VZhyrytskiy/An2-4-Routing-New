1cb5ef76b2a66e2ca0d226269ec5341a240443b8
Vitaliy Zhyrytskyy <Vitaliy_Zhyrytskyy@epam.com>
2018-05-17 13:08:17 +0300

    fix8

--- src/app/tasks/tasks.module.ts
+++ src/app/tasks/tasks.module.ts
@@ -2,19 +2,12 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 
-import { TaskListComponent, TaskComponent, TaskArrayService } from '.';
+import { TasksRoutingModule } from './tasks-routing.module';
+import { TaskComponent, TaskListComponent } from '.';
 
 @NgModule({
-  declarations: [
-    TaskListComponent,
-    TaskComponent
-  ],
-  imports: [
-    CommonModule,
-    FormsModule
-  ],
-  providers: [
-    TaskArrayService
-  ]
+  declarations: [TaskListComponent, TaskComponent],
+  imports: [CommonModule, FormsModule, TasksRoutingModule],
+  providers: []
 })
 export class TasksModule {}
