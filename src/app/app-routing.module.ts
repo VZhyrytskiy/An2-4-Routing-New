21d8cefffd3978de7a276a281671848659a7e76c
Vitaliy Zhyrytskyy <Vitaliy_Zhyrytskyy@epam.com>
2018-06-12 16:53:01 +0300

    fix3

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import { AboutComponent, HomeComponent, PathNotFoundComponent } from './core';
+import { AboutComponent, HomeComponent, PathNotFoundComponent } from './layout';
 
 const routes: Routes = [
   {
