cce13eb3e40ef05b37fbed3aa4a8d35e5cc0bc21
Vitaliy Zhyrytskyy <Vitaliy_Zhyrytskyy@epam.com>
2018-09-21 09:14:03 +0300

    fix2

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { AboutComponent, HomeComponent, PathNotFoundComponent } from '.';
+import {
+  AboutComponent,
+  HomeComponent,
+  PathNotFoundComponent
+} from './components';
 
 @NgModule({
   imports: [CommonModule],
