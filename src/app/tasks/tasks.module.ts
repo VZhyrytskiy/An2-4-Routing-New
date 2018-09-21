b7e2a2b27f5c6aa4cace1263cc72bede1dfd5243
Vitaliy Zhyrytskyy <Vitaliy_Zhyrytskyy@epam.com>
2018-09-21 09:20:04 +0300

    fix8

--- src/app/tasks/tasks.module.ts
+++ src/app/tasks/tasks.module.ts
@@ -3,7 +3,7 @@ import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 
 import { TasksRoutingModule } from './tasks-routing.module';
-import { TaskComponent, TaskListComponent } from '.';
+import { TaskComponent, TaskListComponent } from './components';
 import { TasksServicesModule } from './tasks-services.module';
 
 @NgModule({
