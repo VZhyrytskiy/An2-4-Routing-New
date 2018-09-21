c38c266658a44f2bff9499588ee9e90e3d020471
Vitaliy Zhyrytskyy <Vitaliy_Zhyrytskyy@epam.com>
2018-09-21 09:22:20 +0300

    fix9

--- src/app/tasks/tasks-routing.module.ts
+++ src/app/tasks/tasks-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import { TaskListComponent } from '.';
+import { TaskListComponent } from './components';
 
 const routes: Routes = [
   {
