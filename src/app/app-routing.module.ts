f1987e111308de9b23258be18da72005601b1957
Vitaliy Zhyrytskyy <Vitaliy_Zhyrytskyy@epam.com>
2019-06-07 15:05:12 +0300

    update value for loadChildren option

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -19,11 +19,11 @@ const routes: Routes = [
   },
   {
     path: 'admin',
-    loadChildren: './admin/admin.module#AdminModule'
+    loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule)
   },
   {
     path: 'users',
-    loadChildren: './users/users.module#UsersModule'
+    loadChildren: () => import('./users/users.module').then(m => m.UsersModule)
   },
   {
     path: 'messages',
