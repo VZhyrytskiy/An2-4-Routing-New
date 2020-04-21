216650b54935ccae277e8ad927efb94b9b292b33
Vitaliy Zhyrytskyy <Vitaliy_Zhyrytskyy@epam.com>
2020-04-21 10:32:41 +0300

    ref: update meta content

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
-import { Title } from '@angular/platform-browser';
+import { Title, Meta } from '@angular/platform-browser';
 import { Router, RouterOutlet, NavigationEnd, NavigationStart } from '@angular/router';
 
 // rxjs
@@ -22,7 +22,8 @@ export class AppComponent implements OnInit, OnDestroy {
     private titleService: Title,
     private router: Router,
     public spinnerService: SpinnerService,
-    private preloadingStrategy: CustomPreloadingStrategyService
+    private preloadingStrategy: CustomPreloadingStrategyService,
+    private metaService: Meta
   ) {}
 
   ngOnInit() {
@@ -51,6 +52,7 @@ export class AppComponent implements OnInit, OnDestroy {
     console.log('Activated Component', $event, routerOutlet);
     // another way to set titles
     this.titleService.setTitle(routerOutlet.activatedRouteData.title);
+    this.metaService.addTags(routerOutlet.activatedRouteData.meta);
   }
 
   onDeactivate($event: any, routerOutlet: RouterOutlet) {
